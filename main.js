a997d548990eea2cc1e512ca017b48c13ab6987e
risita2003 <145742619+risita2003@users.noreply.github.com>
2025-07-18 00:40:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed= new Typed(".text",{
+    strings:["Fronted Developer","UI/UX Designer","Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+});
